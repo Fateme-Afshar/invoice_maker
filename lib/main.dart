7bc9951a9339d3a195044e4330140e4af8aeb47f
Fateme-Afshar <fateme.afshar.aghajari@gmail.com>
2024-02-24 17:49:50 +0330

    Creating SignInPage.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-
-// Include the Google Fonts package to provide more text format options
-// https://pub.dev/packages/google_fonts
 import 'package:invoice_maker/sign-in/SignInPage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const InvoiceMakerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InvoiceMakerApp extends StatelessWidget {
+  const InvoiceMakerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +25,45 @@ class MyApp extends StatelessWidget {
           background: Color.fromARGB(255, 250, 249, 246),
           brightness: Brightness.light,
         ),
+        fontFamily: "segoe",
+        inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: Color.fromARGB(255, 250, 249, 246),
+            hoverColor: Colors.transparent,
+            focusColor: null,
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 0, 0, 128), width: 1),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 0, 0, 128), width: 1),
+            ),
+            hintStyle: TextStyle(color: Color.fromARGB(189, 47, 79, 79))),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          textStyle: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          backgroundColor: const Color.fromARGB(255, 0, 0, 128),
+          foregroundColor: const Color.fromARGB(255, 255, 127, 80),
+        )),
         textTheme: const TextTheme(
-          displayLarge:  TextStyle(
-            fontFamily: "segoe",
+          bodyMedium: TextStyle(
+              color: Color.fromARGB(255, 47, 79, 79),
+              fontSize: 18,
+              fontWeight: FontWeight.bold),
+          displayLarge: TextStyle(
               fontSize: 72,
               fontWeight: FontWeight.bold,
               color: Color.fromARGB(255, 47, 79, 79)),
-          displayMedium: TextStyle(
-            fontFamily: "segoe",
-            fontSize: 25,
-            color: Color.fromARGB(255, 47, 79, 79)
-          ),
-          displaySmall: TextStyle(
-            fontFamily: "segoe",
-            fontSize: 25,
-            color: Color.fromARGB(255, 47, 79, 79)
-        ),
+          displayMedium:
+              TextStyle(fontSize: 25, color: Color.fromARGB(255, 47, 79, 79)),
+          displaySmall:
+              TextStyle(fontSize: 25, color: Color.fromARGB(255, 47, 79, 79)),
         ),
       ),
       home: const MainPage(),
@@ -51,12 +71,11 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget{
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const SignInPage();
   }
-
 }
--- lib/sign-in/SignInPage.dart
+++ lib/sign-in/SignInPage.dart
@@ -1,16 +1,63 @@
- import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SignInPage extends StatelessWidget{
+class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-      return  Scaffold(
-        body: ElevatedButton(onPressed: (){
-
-        }, child: Text("Login page"),),
-      );
+    return Scaffold(
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          constraints: BoxConstraints(
+            maxWidth: 400,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 16),
+                child: Text(
+                  "Sign In your account",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: "Username/ Email"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: "Password"),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign In",
+                    )),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 24, 0, 16),
+                  child: Text(
+                    "create an account",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
-
 }
