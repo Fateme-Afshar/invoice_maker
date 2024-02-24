344927d978f3315fcc26a8ba6a605fb2dc096179
Fateme-Afshar <fateme.afshar.aghajari@gmail.com>
2024-02-24 15:30:55 +0330

    Adding theme color and fonts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// Include the Google Fonts package to provide more text format options
+// https://pub.dev/packages/google_fonts
+import 'package:invoice_maker/sign-in/SignInPage.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,95 +11,52 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const appName = 'Custom Themes';
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: appName,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 0, 0, 128),
+          primary: const Color.fromARGB(255, 0, 0, 128),
+          primaryContainer: Color.fromARGB(255, 255, 127, 80),
+          secondary: const Color.fromARGB(255, 0, 0, 128),
+          secondaryContainer: const Color.fromARGB(255, 255, 127, 80),
+          background: Color.fromARGB(255, 250, 249, 246),
+          brightness: Brightness.light,
+        ),
+        textTheme: const TextTheme(
+          displayLarge:  TextStyle(
+            fontFamily: "segoe",
+              fontSize: 72,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 47, 79, 79)),
+          displayMedium: TextStyle(
+            fontFamily: "segoe",
+            fontSize: 25,
+            color: Color.fromARGB(255, 47, 79, 79)
+          ),
+          displaySmall: TextStyle(
+            fontFamily: "segoe",
+            fontSize: 25,
+            color: Color.fromARGB(255, 47, 79, 79)
+        ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const MainPage(),
     );
   }
+}
+
+class MainPage extends StatelessWidget{
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SignInPage();
+  }
+
 }
---
+++ lib/sign-up/SignUpPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget{
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: ElevatedButton(onPressed: (){
+
+      }, child: Text("Login page"),),
+    );
+  }
+
+}
